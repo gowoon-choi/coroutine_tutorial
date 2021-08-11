761e0d5c1d61365dcc8c1ae06e54a8a67e4f7c66
gowoonchoi <gowoonvv@gmail.com>
2021-08-11 19:36:10 +0900

    init

---
+++ coroutine_tutorial_android/app/src/main/java/com/golucky/coroutinetutorial_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.golucky.coroutinetutorial_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ coroutine_tutorial_kotlin/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "CoroutineTutorial_Kotlin"
+
