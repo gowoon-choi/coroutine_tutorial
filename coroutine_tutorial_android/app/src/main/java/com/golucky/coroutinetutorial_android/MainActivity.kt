03bad5c479e06edaac9265ecd02b358e6c658d8c
gowoonchoi <gowoonvv@gmail.com>
2021-08-25 06:09:58 +0900

    get news from coroutine scope with ServiceCall Thread

--- coroutine_tutorial_android/app/src/main/java/com/golucky/coroutinetutorial_android/MainActivity.kt
+++ coroutine_tutorial_android/app/src/main/java/com/golucky/coroutinetutorial_android/MainActivity.kt
@@ -2,17 +2,39 @@ package com.golucky.coroutinetutorial_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
     private val dispatcher = newSingleThreadContext(name = "ServiceCall")
+    private val factory  = DocumentBuilderFactory.newInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         GlobalScope.launch(dispatcher) {
+            val headlines = fetchRssHeadlines()
+            val newsCount = findViewById<TextView>(R.id.newsCount)
+//            newsCount.text = "Found ${headlines.size} News"
 
         }
+    }
+
+    private fun fetchRssHeadlines() : List<String>{
+        val builder = factory.newDocumentBuilder()
+        val xml = builder.parse("https://www.npr.org/rss/rss.php?id=1001")
+        val news = xml.getElementsByTagName("channel").item(0)
+        return (0 until news.childNodes.length)
+            .map { news.childNodes.item(it) }
+            .filter { Node.ELEMENT_NODE == it.nodeType }
+            .map { it as Element }
+            .filter { "item" == it.tagName }
+            .map {
+                it.getElementsByTagName("title").item(0).textContent
+            }
     }
 }
